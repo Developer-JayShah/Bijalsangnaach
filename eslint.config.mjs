01e7adf0e0c961f53dd2014c4c9d05b7d381c5ba
Developer-JayShah <59352098+Developer-JayShah@users.noreply.github.com>
2025-12-26 23:20:25 -0500

    Fix: correct flat eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,24 +2,17 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+
+  // ignore build folders
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+
+  // custom rules
   {
-  "extends": ["next/core-web-vitals", "next/typescript"],
-  "rules": {
-    "@typescript-eslint/no-explicit-any": "off"
-  }
-}
-
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ]);
-
-export default eslintConfig;
